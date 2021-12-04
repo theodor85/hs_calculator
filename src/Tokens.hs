0bed0c4c62c563bdb20fe20615118bf57cb51dc6
Teddy Coder <fedor_coder@mail.ru>
2021-12-04 17:28:09 +0700

    add tokenization

--- src/Tokens.hs
+++ src/Tokens.hs
@@ -3,36 +3,53 @@ module Tokens
       Token
     ) where
 
-import Data.Char
-import Control.Monad (when)
+import Data.Char ( isDigit )
 
 
 data Token = Plus | Minus | Div | Mult | LeftParenthesis | RightParenthesis | Value Int
   deriving Show
 
 tokenize :: String -> [Token]
-tokenize [] = []
+tokenize []         = []
 tokenize expression = fst clearAndCreateToken : tokenize (snd clearAndCreateToken)
   where
     clearAndCreateToken = createToken $ clearSpace expression
 
 createToken :: String -> (Token, String)
-createToken expression = 
-  if isDigit(head expression)
+createToken expression =
+  let exp_head = head expression
+      exp_tail = tail expression
+  in
+    if isDigit(head expression)
+    then
+      createValue expression
+    else
+      case head expression of
+        '+' -> (Plus,             exp_tail)
+        '*' -> (Mult,             exp_tail)
+        '/' -> (Div,              exp_tail)
+        '-' -> (Minus,            exp_tail)
+        '(' -> (LeftParenthesis,  exp_tail)
+        ')' -> (RightParenthesis, exp_tail)
+        _   -> error "Wrong Symbol!"
+
+createValue :: String -> (Token, String)
+createValue exp =
+  let (rest_exp, digits) = getDigits exp ""
+  in
+    (Value (read digits), rest_exp)
+
+getDigits :: String -> String -> (String, String)
+getDigits ""  result = ("", result)
+getDigits str result =
+  if isDigit (head str)
   then
-    (Value (read [head expression]), tail expression)
+    getDigits (tail str) (result ++ [head str])
   else
-    case head expression of
-      '+' -> (Plus,             tail expression)
-      '*' -> (Mult,             tail expression)
-      '/' -> (Div,              tail expression)
-      '-' -> (Minus,            tail expression)
-      '(' -> (LeftParenthesis,  tail expression)
-      ')' -> (RightParenthesis, tail expression)
-      _   -> error "Wrong Symbol!"
+    (str, result)
 
 clearSpace :: String -> String
-clearSpace [] = []
+clearSpace []  = []
 clearSpace str = if head str == ' '
                  then clearSpace(tail str)
                  else head str : clearSpace(tail str)
